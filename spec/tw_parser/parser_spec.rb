52dc71b8a87188649626a19569bd7453ac775cd8
okonomi <okonomi@oknm.jp>
2025-08-16 13:29:51 +0900

    refactor: pair expected and actual

--- spec/tw_parser/parser_spec.rb
+++ spec/tw_parser/parser_spec.rb
@@ -4,48 +4,35 @@ RSpec.describe TwParser::Parser do
   describe "#parse" do
     subject { described_class.new.parse(input) }
 
-    context "should parse a simple utility" do
-      let(:input) { "flex" }
-
-      it {
-        is_expected.to eq(TwParser::StaticCandidate.new(
-                            important: false,
-                            raw: "flex",
-                            root: "flex",
-                            variants: []
-                          ))
-      }
-    end
-
-    context "should parse a simple utility that should be important" do
-      let(:input) { "flex!" }
-
-      it {
-        is_expected.to eq(TwParser::StaticCandidate.new(
-                            important: true,
-                            raw: "flex!",
-                            root: "flex",
-                            variants: []
-                          ))
-      }
-    end
-
-    context "should parse a simple utility that can be negative" do
-      let(:input) { "-translate-x-4" }
-
-      it {
-        is_expected.to eq(TwParser::FunctionalCandidate.new(
-                            important: false,
-                            modifier: nil,
-                            raw: "-translate-x-4",
-                            root: "-translate-x",
-                            value: TwParser::NamedUtilityValue.new(
-                              fraction: nil,
-                              value: "4"
-                            ),
-                            variants: []
-                          ))
-      }
+    {
+      "flex" => TwParser::StaticCandidate.new(
+        important: false,
+        raw: "flex",
+        root: "flex",
+        variants: []
+      ),
+      "flex!" => TwParser::StaticCandidate.new(
+        important: true,
+        raw: "flex!",
+        root: "flex",
+        variants: []
+      ),
+      "-translate-x-4" => TwParser::FunctionalCandidate.new(
+        important: false,
+        modifier: nil,
+        raw: "-translate-x-4",
+        root: "-translate-x",
+        value: TwParser::NamedUtilityValue.new(
+          fraction: nil,
+          value: "4"
+        ),
+        variants: []
+      )
+    }.each do |input, expected|
+      context input do
+        let(:input) { input }
+        it { is_expected.to eq(expected) }
+      end
     end
   end
 end
