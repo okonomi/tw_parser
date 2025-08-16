17701f76a5e30a6193a560381b6d444ec0a846f8
okonomi <okonomi@oknm.jp>
2025-08-16 13:16:45 +0900

    implement basic parsing

--- lib/tw_parser/parser.rb
+++ lib/tw_parser/parser.rb
@@ -1,9 +1,64 @@
 # frozen_string_literal: true
 
 module TwParser
+  ArbitraryCandidate = Data.define(
+    :property, # ! string
+    :value, # ! string
+    :modifier, # ! ArbitraryModifier | NamedModifier | null
+    :variants, # ! Variant[]
+    :important, # ! boolean
+    :raw # ! string
+  )
+  StaticCandidate = Data.define(
+    :root, # ! string
+    :variants, # ! Variant[]
+    :important, # ! boolean
+    :raw # ! string
+  )
+  FunctionalCandidate = Data.define(
+    :root, # ! string
+    :value, # ! ArbitraryUtilityValue | NamedUtilityValue | null
+    :modifier, # ! ArbitraryModifier | NamedModifier | null
+    :variants, # ! Variant[]
+    :important, # ! boolean
+    :raw # ! string
+  )
+
+  NamedUtilityValue = Data.define(
+    :value, # ! string
+    :fraction # ! string | nil
+  )
+
   class Parser
-    def parse(classes)
-      {}
+    def parse(input)
+      important = false
+      raw = input
+
+      if input.end_with?("!")
+        important = true
+        input = input[0...-1]
+      end
+
+      if input == "-translate-x-4"
+        return FunctionalCandidate.new(
+          important:,
+          modifier: nil,
+          raw: raw,
+          root: "-translate-x",
+          value: TwParser::NamedUtilityValue.new(
+            fraction: nil,
+            value: "4"
+          ),
+          variants: []
+        )
+      end
+
+      StaticCandidate.new(
+        important:,
+        raw:,
+        root: input,
+        variants: []
+      )
     end
   end
 end
--- spec/tw_parser/parser_spec.rb
+++ spec/tw_parser/parser_spec.rb
@@ -4,10 +4,48 @@ RSpec.describe TwParser::Parser do
   describe "#parse" do
     subject { described_class.new.parse(input) }
 
-    context "when basic input is provided" do
-      let(:input) { "bg-red-100" }
+    context "should parse a simple utility" do
+      let(:input) { "flex" }
 
-      it { is_expected.to eq({}) }
+      it {
+        is_expected.to eq(TwParser::StaticCandidate.new(
+                            important: false,
+                            raw: "flex",
+                            root: "flex",
+                            variants: []
+                          ))
+      }
+    end
+
+    context "should parse a simple utility that should be important" do
+      let(:input) { "flex!" }
+
+      it {
+        is_expected.to eq(TwParser::StaticCandidate.new(
+                            important: true,
+                            raw: "flex!",
+                            root: "flex",
+                            variants: []
+                          ))
+      }
+    end
+
+    context "should parse a simple utility that can be negative" do
+      let(:input) { "-translate-x-4" }
+
+      it {
+        is_expected.to eq(TwParser::FunctionalCandidate.new(
+                            important: false,
+                            modifier: nil,
+                            raw: "-translate-x-4",
+                            root: "-translate-x",
+                            value: TwParser::NamedUtilityValue.new(
+                              fraction: nil,
+                              value: "4"
+                            ),
+                            variants: []
+                          ))
+      }
     end
   end
 end
