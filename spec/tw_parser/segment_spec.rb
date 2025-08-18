c5bfa673d8995b8d6a5ee87521dd9f4e9d01a182
okonomi <okonomi@oknm.jp>
2025-08-18 21:40:17 +0900

    test: add incorrect cases

--- spec/tw_parser/segment_spec.rb
+++ spec/tw_parser/segment_spec.rb
@@ -6,20 +6,36 @@ RSpec.describe TwParser do
   describe ".segment" do
     subject { described_class.segment(input, delimiter) }
 
-    {
-      "foo" => %w[foo],
-      "foo:bar:baz" => %w[foo bar baz],
-      "a:(b:c):d" => %w[a (b:c) d],
-      "a:[b:c]:d" => %w[a [b:c] d],
-      "a:{b:c}:d" => %w[a {b:c} d],
-      'a:"b:c":d' => ["a", '"b:c"', "d"],
-      "a:'b:c':d" => ["a", "'b:c'", "d"]
-    }.each do |input, expected|
-      context "when #{input}" do
-        let(:input) { input }
-        let(:delimiter) { ":" }
+    context "when input is correct" do
+      {
+        "foo" => %w[foo],
+        "foo:bar:baz" => %w[foo bar baz],
+        "a:(b:c):d" => %w[a (b:c) d],
+        "a:[b:c]:d" => %w[a [b:c] d],
+        "a:{b:c}:d" => %w[a {b:c} d],
+        'a:"b:c":d' => ["a", '"b:c"', "d"],
+        "a:'b:c':d" => ["a", "'b:c'", "d"]
+      }.each do |input, expected|
+        context "with #{input}" do
+          let(:input) { input }
+          let(:delimiter) { ":" }
 
-        it { is_expected.to eq(expected) }
+          it { is_expected.to eq(expected) }
+        end
+      end
+    end
+
+    context "when input is incorrect" do
+      {
+        'a:"b:c:d' => ["a", '"b:c:d'],
+        "a:'b:c:d" => ["a", "'b:c:d"]
+      }.each do |input, expected|
+        context "with #{input}" do
+          let(:input) { input }
+          let(:delimiter) { ":" }
+
+          it { is_expected.to eq(expected) }
+        end
       end
     end
   end
