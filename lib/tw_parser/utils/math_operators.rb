4e838c8427a922216550a66ccd63ca002f88ffd8
okonomi <okonomi@oknm.jp>
2025-09-11 23:22:54 +0900

    feat(math_operators): skip when math functions are not included

--- lib/tw_parser/utils/math_operators.rb
+++ lib/tw_parser/utils/math_operators.rb
@@ -8,9 +8,13 @@ module TwParser
     module MathOperators
       OPERATORS = %w[+ - * /].freeze
 
+      MATH_FUNCTIONS = %w[calc].freeze
+
       class << self
         #: (String input) -> String
         def add_whitespace(input)
+          return input unless input.match?(Regexp.union(*MATH_FUNCTIONS))
+
           result = +""
 
           scan = StringScanner.new(input)
--- spec/tw_parser/utils/math_operators_spec.rb
+++ spec/tw_parser/utils/math_operators_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe TwParser::Utils::MathOperators do
     subject { described_class.add_whitespace(input) }
 
     {
-      "1+1" => "1 + 1",
-      "100%-4px" => "100% - 4px",
+      "calc(1+1)" => "calc(1 + 1)",
+      "calc(100%-4px)" => "calc(100% - 4px)",
       "var(--foo)" => "var(--foo)"
       # "calc(100% - 4px)" => "calc(100% - 4px)",
       # "var(--foo+bar)" => "var(--foo+bar)",
