8c95628158e240c6a1e9b0ce8c76f73109abf23d
okonomi <okonomi@oknm.jp>
2025-09-12 18:14:46 +0900

    feat(math_operators): scan tokens

--- lib/tw_parser/utils/math_operators.rb
+++ lib/tw_parser/utils/math_operators.rb
@@ -19,13 +19,24 @@ module TwParser
 
           scanner = StringScanner.new(input)
           until scanner.eos?
-            result << if scanner.scan(/\d+[%a-z]*/)
-                        scanner.matched || ""
-                      elsif scanner.scan(Regexp.union(*OPERATORS))
-                        " #{scanner.matched} "
-                      else
-                        scanner.getch || ""
-                      end
+            case # rubocop:disable Style/EmptyCaseCondition,Style/ConditionalAssignment
+            # function start
+            when scanner.scan(/(?:#{MATH_FUNCTIONS.join("|")})\(/)
+              # TODO: stack push
+              result << scanner.matched.to_s
+            # function end
+            when scanner.scan(/\)/) # rubocop:disable Lint/DuplicateBranch,Style/RedundantRegexpArgument
+              # TODO: stack pop
+              result << scanner.matched.to_s
+            # value
+            when scanner.scan(/\d+[%a-z]*/) # rubocop:disable Lint/DuplicateBranch
+              result << scanner.matched.to_s
+            # operator
+            when scanner.scan(Regexp.union(*OPERATORS))
+              result << " #{scanner.matched} "
+            else
+              result << scanner.getch.to_s
+            end
           end
 
           result
